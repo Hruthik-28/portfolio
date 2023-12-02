3a89513fc3c7c2951d53ed78ad67c415b126b1c6
HRUTHIK <hruthik9980819630@gmail.com>
2023-12-02 20:27:55 +0530

    changed images path

--- src/components/ProjectListing.jsx
+++ src/components/ProjectListing.jsx
@@ -6,21 +6,21 @@ function ProjectListing() {
             icon: "https://uploads-ssl.webflow.com/64470065adf2a67f8abcbb4d/64470065adf2a66d02bcbb81_thumbnial-shoppop.svg",
             title: "Just Blog It", 
             keyword: "React, Redux, Appwrite, Authentication, File Management ", 
-            image: "../../public/astronaut.jpg",
+            image: "/public/astronaut.jpg",
             bgColor: "#0084FF"
         },
         {
             icon: "https://uploads-ssl.webflow.com/64470065adf2a67f8abcbb4d/64470065adf2a6125fbcbb83_thumbnial-byteleaders.svg", 
             title: "Todo Breeze", 
             keyword: "React, ContextApi, Local Storage, Tailwind", 
-            image: "../../public/astronaut.jpg",
+            image: "/public/astronaut.jpg",
             bgColor: "#f1c40f" 
         },
         {
             icon: "https://uploads-ssl.webflow.com/64470065adf2a67f8abcbb4d/64470065adf2a6168bbcbb82_thumbnial-kortinq.svg", 
             title: "Todo Breeze", 
             keyword: "React, ContextApi, Local Storage, Tailwind", 
-            image: "../../public/astronaut.jpg",
+            image: "/public/astronaut.jpg",
             bgColor: "#501028" 
         },
     ]
